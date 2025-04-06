84c4cac8169b08f71a5f1c067acf717117be2cdc
Nicolas Lanker <Stigz@users.noreply.github.com>
2025-04-06 18:17:39 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,11 @@ import (
 var templates *template.Template
 
 func main() {
-	templates = template.Must(template.ParseGlob("templates/*.html"))
+	var err error
+	templates, err = template.ParseGlob("templates/*.html")
+	if err != nil {
+		log.Fatalf("Error parsing templates: %v", err)
+	}
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", homeHandler)
@@ -17,15 +21,24 @@ func main() {
 	http.HandleFunc("/submit-booking", submitBookingHandler)
 
 	log.Println("Server running at http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "home.html", nil)
+	err := templates.ExecuteTemplate(w, "home.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func bookHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "book.html", nil)
+	err := templates.ExecuteTemplate(w, "book.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func submitBookingHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,4 +52,4 @@ func submitBookingHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("Booking received: %s, %s, %s", name, email, dates)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
-} 
+}
